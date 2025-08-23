9a0b25cab86630473c1dbe235e8470d75edc585c
kelocoes <kevin.rodriguez109@gmail.com>
2025-08-23 17:41:42 -0500

    Add Jest

---
+++ jest.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+    clearMocks: true,
+    collectCoverage: true,
+    coverageDirectory: "coverage",
+    coverageProvider: "v8",
+    preset: "ts-jest",
+    testEnvironment: "node",
+    roots: ["./src"],
+    transform: {
+        "^.+\\.ts?$": "ts-jest"
+    },
+    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.ts$",
+    moduleFileExtensions: ["ts", "js", "json", "node"],
+    collectCoverageFrom: [
+        "src/**/*.ts", // Incluye todos los archivos TypeScript en src/
+        "!src/**/*.d.ts", // Excluye archivos de declaración
+        "!src/index.ts", // Excluye el archivo de entrada principal
+        "!src/interfaces/**/*.ts", // Excluye interfaces
+        "!src/config/**/*.ts", // Excluye archivos de configuración
+        "!src/models/**/*.ts", // Excluye models
+        "!src/routes/**/*.ts", // Excluye utilidades
+        "!src/**/index.ts", // Excluye archivos index
+        "!src/validators/**/*.ts", // Excluye validadores
+    ],
+};
+
+export default config;
---
+++ src/tests/utils/operation.util.test.ts
@@ -0,0 +1,5 @@
+import { add } from "../../utils";
+
+it("should add two numbers", () => {
+    expect(add(2, 3)).toBe(5);
+});
