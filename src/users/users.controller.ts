9a257803ac62471d6fe0f8ae16e3bd886a10d867
kelocoes <kevin.rodriguez109@gmail.com>
2025-09-20 16:52:03 -0500

    Nest controllers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,37 +6,65 @@ import {
     Patch,
     Param,
     Delete,
+    InternalServerErrorException,
+    Header,
+    Headers,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { HttpCode, HttpStatus } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
     @Post()
+    @HttpCode(HttpStatus.CREATED)
     create(@Body() createUserDto: CreateUserDto) {
         return this.usersService.create(createUserDto);
     }
 
     @Get()
+    @HttpCode(HttpStatus.OK)
     findAll() {
         return this.usersService.findAll();
     }
 
     @Get(':id')
+    @HttpCode(HttpStatus.OK)
     findOne(@Param('id') id: string) {
         return this.usersService.findOne(+id);
     }
 
     @Patch(':id')
-    update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-        return this.usersService.update(+id, updateUserDto);
+    @HttpCode(HttpStatus.OK)
+    async update(
+        @Param('id') id: string,
+        @Body() updateUserDto: UpdateUserDto,
+    ) {
+        try {
+            return await this.usersService.update(+id, updateUserDto);
+        } catch (error) {
+            throw new InternalServerErrorException('Failed to update user', {
+                cause: error,
+                description:
+                    'An error occurred while updating the user in the database.',
+            });
+        }
     }
 
     @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.usersService.remove(+id);
+    @HttpCode(HttpStatus.NO_CONTENT)
+    async remove(@Param('id') id: string) {
+        await this.usersService.remove(+id);
+    }
+
+    @Get('test/info')
+    @HttpCode(HttpStatus.AMBIGUOUS)
+    @Header('Custom-Header', 'CustomHeaderValue')
+    test(@Headers('Content-Type') contentType: string) {
+        console.log('Content-Type:', contentType);
+        return 'Test endpoint';
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,6 +5,10 @@ import { User } from './entities/user.entity';
 import { RolesService } from '../auth/services/roles.service';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import {
+    RoleNotFoundException,
+    UserNotFoundException,
+} from '../common/exceptions';
 
 @Injectable()
 export class UsersService {
@@ -17,7 +21,7 @@ export class UsersService {
     async create(createUserDto: CreateUserDto) {
         const role = await this.rolesService.findByName(createUserDto.roleName);
         if (!role) {
-            throw new Error('Role not found');
+            throw new RoleNotFoundException(createUserDto.roleName);
         }
 
         const newUser = this.userRepository.create({
@@ -31,20 +35,27 @@ export class UsersService {
         return this.userRepository.find();
     }
 
-    findOne(id: number) {
-        return this.userRepository.findOne({ where: { id } });
+    async findOne(id: number) {
+        const user = await this.userRepository.findOne({ where: { id } });
+        if (!user) {
+            throw new UserNotFoundException(id);
+        }
+        return user;
     }
 
     async update(id: number, updateUserDto: UpdateUserDto) {
+        const userExist = await this.userRepository.findOne({ where: { id } });
+        if (!userExist) {
+            throw new UserNotFoundException(id, 'P20250');
+        }
         await this.userRepository.update(id, updateUserDto);
         return this.findOne(id);
     }
 
     async remove(id: number) {
         const result = await this.userRepository.delete(id);
-        if (result.affected) {
-            return { id };
+        if (!result.affected || result.affected === 0) {
+            throw new UserNotFoundException(id);
         }
-        return null;
     }
 }
