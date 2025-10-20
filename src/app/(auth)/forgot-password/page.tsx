d06be9255ba602ca5cb384e0d17e3539f9c9eab6
kelocoes <kevin.rodriguez109@gmail.com>
2025-10-20 09:20:45 -0500

    Next intro

---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,10 @@
+export default function ForgotPasswordPage() {
+    return (
+        <div id="forgot-password-container" className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 w-86">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" className="mx-auto h-10 w-auto" />
+                <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Reset your password</h2>
+            </div>
+        </div>
+    );
+}
