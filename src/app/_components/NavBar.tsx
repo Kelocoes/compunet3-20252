60b240ee0b5dbe5a10be850c953b84b2940c1c56
kelocoes <kevin.rodriguez109@gmail.com>
2025-10-30 11:07:44 -0500

    Context

--- src/app/_components/NavBar.tsx
+++ src/app/_components/NavBar.tsx
@@ -13,6 +13,7 @@ export default function NavBar() {
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                         <li><Link href="/dashboard/games">Games</Link></li>
                         <li><Link href="/dashboard/users">Profile</Link></li>
+                        <li><Link href="/dashboard/context">Context</Link></li>
                     </ul>
                 </div>
             </div>
